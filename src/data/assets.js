5bb09e2249c3efcd9bb24935f95eabf768255dfa
mymundial <alexjashworth@gmail.com>
2026-05-21 11:46:16 +0100

    Delete src directory

--- src/data/assets.js
+++
@@ -1,5 +0,0 @@
-export const ASSETS = {
-  ballerLogo: "https://ballerleague.uk/assets/front_img/logo-baller-icon.svg",
-  mondayLogo: "https://ballerleague.uk/assets/front_img/logo-baller-icon.svg",
-  myMundialLogo: "https://ballerleague.uk/assets/front_img/logo-baller-icon.svg",
-};
